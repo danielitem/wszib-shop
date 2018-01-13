f31628967d842c0023ce6b592a71a0b564f8a839
Daniel Grzegorz Dybek <ddybek@student.wszib.edu.pl>
2018-01-13 15:04:31 +0100

    Add products

--- src/Shop.Web/Controllers/ProductsController.cs
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -4,22 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Core.Domain;
-
+using Shop.Web.Models;
 
 namespace Shop.Web.Controllers
 {
+    [Route("products")]
     public class ProductsController : Controller
     {
         private static readonly List<Product> _products = new List<Product>
         {
             new Product("Laptop", "Electronics", 2999),
-            new Product("Trousers", "Clothes", 110),
+            new Product("Jeans", "Trousers", 110),
             new Product("Hammer", "Tools", 29),
         };
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_products);
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            var viewModel = new ProductViewModel();
+            return View(viewModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(ProductViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            _products.Add(new Product(viewModel.Name, viewModel.Category, viewModel.Price));
+
+            return RedirectToAction(nameof(Index));
+            
         }
     }
 }
---
+++ src/Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Models
+{
+    public class ProductViewModel //model do dodawania produktów
+    {
+        [Required(ErrorMessage = "Nazwa musi zostać podana.")]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(1,100000)]
+        public decimal Price { get; set; }
+
+        public List<SelectListItem> Categories { get; } = new List<SelectListItem> // lista kategorii
+        {
+            new SelectListItem {Text="Electronics", Value = "Electronics"},
+            new SelectListItem {Text="Tools", Value = "Tools"},
+            new SelectListItem {Text="Trousers", Value = "Trousers"}
+        };
+    }
+}
