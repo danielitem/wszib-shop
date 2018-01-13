58b6e70dd1d99f553c2a3aef478ba46b5c8f8b88
Daniel Grzegorz Dybek <ddybek@student.wszib.edu.pl>
2018-01-13 12:43:25 +0100

    Added products page

---
+++ src/Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Core.Domain;
+
+
+namespace Shop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static readonly List<Product> _products = new List<Product>
+        {
+            new Product("Laptop", "Electronics", 2999),
+            new Product("Trousers", "Clothes", 110),
+            new Product("Hammer", "Tools", 29),
+        };
+
+        public IActionResult Index()
+        {
+            return View(_products);
+        }
+    }
+}
